65151341913f4ad729fa68a86e57e55cc0f6197a
Eswar279 <125408875+Eswar279@users.noreply.github.com>
2023-10-30 23:14:38 -0400

    Delete hw3-the second phase of your web app directory

--- hw3-the second phase of your web app/Mamillapalli-EswarasanthoshKumar_hw3_secondPhaseOfApp_upload.php
+++
@@ -1,8 +0,0 @@
-<? php
-header("Access-Control-Allow-Origin: *");
-$filename=$_FILES['file']['name'];
-$location="\Users\eswarsanthoshkumarmamillapalli\Desktop\IWS\upload/"+.$filename;
-if(move_uploaded_file($_FILES['file']['tmp_name'],$location)){
-    echo 'success'
-}else{echo 'false'}
-?>
